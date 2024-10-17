be113abbad3075ad4a98358b7b21fb451a1f5303
aashishthakurcodes <j9183208@gmail.com>
2024-10-17 08:48:03 +0530

    Removing Comment

--- JS_Day/Day_02/script.js
+++ JS_Day/Day_02/script.js
@@ -25,18 +25,7 @@ let number1=8650031;
 let number2=5373;
 console.log("Remainder:-",number1%number2)
 
-
-// Activity 2: Assignment Operators
-// Task 6: Use the += operator to add a number to a variable and log the result to the console.
-// Task 7: Use the -= operator to subtract a number from a variable and log the result to the console.
-// Activity 3: Comparison Operators
-// Task 8: Write a program to compare two numbers using > and < and log the result to the console.
-// Task 9: Write a program to compare two numbers using >= and <= and log the result to the console.
-// Task 10: Write a program to compare two numbers using == and === and log the result to the console.
-// Activity 4: Logical Operators
-// Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console.
-// Task 12: Write a program that uses the || operator to combine two conditions and log the result to the console.
-// Task 13: Write a program that uses the ! operator to negate a condition and log the result to the console.
+//Activity 2: Assignment Operator
 
 // Task 6: Use the += operator to add a number to a variable and log the result to the console.
 let val1=5673;
@@ -48,7 +37,9 @@ let val2=974675;
 val2-=78730;
 console.log("Subtraction using -= operator",val2)
 
+
 // Activity 3: Comparison Operators
+
 // Task 8: Write a program to compare two numbers using > and < and log the result to the console.
 let val3=579886;
 let val4=987665;
@@ -70,7 +61,9 @@ let str2="Rahul";
 console.log(`Is ${str1} === ${str2}`, str1===str2)
 // In === value, datatype should be same
 
+
 // Activity 4: Logical Operators
+
 // Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console.
 
 // &&LOGICAL AND OPERATOR
@@ -81,12 +74,12 @@ if(value>=0 && value%2==0){
 else{
     console.log("Not even number")
 }
+
 // Task 12: Write a program that uses the || operator to combine two conditions and log the result to the console.
 // ||LOGICAL OR OPERATOR
 let x=30;
 let y=23;
 console.log(x==40 || y>30)
-
 
 
 // Task 13: Write a program that uses the ! operator to negate a condition and log the result to the console.
@@ -95,13 +88,18 @@ let atr1=34
 let atr2=55;
 console.log(!(atr2>atr1));
 
+
+
 // Activity 5: Ternary Operator
+
 // Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
 //Condition ? Value if true : Value if False
 let atr3=735;
 console.log((20>98 ?"Yes":"No"))
 
+
 // Feature Request:
+
 // Arithmetic Operations Script: Write a script that performs basic arithmetic operations (addition, subtraction, multiplication, division, remainder) on two numbers and logs the results.
 
 let nm1=458759;
@@ -114,6 +112,7 @@ console.log(`Division of ${nm1} and ${nm2}`, nm1/nm2);
 console.log(`Remainder of ${nm1} and ${nm2}`, nm1%nm2);
 
 // Comparison and Logical Operators Script: Create a script that compares two numbers using different comparison operators and combines conditions using logical operators, logging the results.
+
 let value1=30;
 let value2=56;
 console.log(`Is ${value1} > than ${value2}`, value1>value2)
